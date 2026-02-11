5cc4f99bc1c14990c8bd99edd5cca6c2401a43d0
ChatonnetLilou <lchatonnet@gaming.tech>
2026-02-11 16:33:04 +0100

    xdvg

--- src/sandbox/pch.h
+++ src/sandbox/pch.h
@@ -4,6 +4,5 @@
 #pragma comment (lib,"../hlt_render/x64/Debug/hlt_render.lib")
 #pragma comment (lib,"../hlt_engine/x64/Debug/hlt_engine.lib")
 #pragma comment (lib,"../hlt_core/x64/Debug/hlt_core.lib")
-#include <windows.h>
 
 #endif
