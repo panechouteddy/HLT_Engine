f2cfc8ea487894e1810fb5d311bfe1a8275de9a4
Hugo PONTHIEU <hponthieu@gaming.tech>
2026-03-05 14:57:40 +0100

    obj Importer

--- src/hlt_render/hlt_CustomModelImporter.h
+++ src/hlt_render/hlt_CustomModelImporter.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "D3DApp.h"
 
+struct face
+{
+    int vertexID[3];
+    int uvID[3];
+    int normalID[3];
+};
+
 namespace hlt_ModelImporter
 {
 	static void ImportOBJ(const std::string& path)
@@ -8,8 +15,10 @@ namespace hlt_ModelImporter
         std::ifstream file(path);
         std::string line;
 
-        std::vector<Vertex> vertices;
-        std::vector<uint16_t> indices;
+        std::vector<XMFLOAT3> vertexPos;
+        std::vector<XMFLOAT2> uv;
+        std::vector<XMFLOAT3> normal;
+        std::vector<face> faces;
 
         while (std::getline(file, line))
         {
@@ -19,23 +28,69 @@ namespace hlt_ModelImporter
 
             if (head == "v")
             {
-                Vertex v{};
-                readLine >> v.Pos.x >> v.Pos.y >> v.Pos.z;
-                vertices.push_back(v);
+                XMFLOAT3 vPos;
+                readLine >> vPos.x >> vPos.y >> vPos.z;
+                vertexPos.push_back(vPos);
             }
+            else if (head == "vt")
+            {
+                XMFLOAT2 vUV;
+                readLine >> vUV.x >> vUV.y;
+                uv.push_back(vUV);
+            }
+            else if (head == "vn")
+            {
+                XMFLOAT3 vNormal;
+                readLine >> vNormal.x >> vNormal.y >> vNormal.z;
+                normal.push_back(vNormal);
+            }
+
             else if (head == "f")
             {
                 std::string vertexData;
+                face currentFace{};
+                int vertexIndex = 0;
                 while (readLine >> vertexData) {
                     // On remplace les '/' par des espaces pour lire facilement avec un stringstream
                     for (char& c : vertexData) if (c == '/') c = ' ';
 
                     std::stringstream vss(vertexData);
-                    int indice = -1;
-                    vss >> indice;
-                    if (indice >= 0)
-                        indices.push_back(static_cast<uint16_t>(indice));
+                    int vertexID = -1, uvID = -1, normalID = -1;
+                    vss >> vertexID >> uvID >> normalID;
+                    if (vertexID >= 0)
+                    {
+                        currentFace.vertexID[vertexIndex] = static_cast<uint16_t>(vertexID);
+                        if (uvID >= 0)
+                        {
+                            currentFace.uvID[vertexIndex] = static_cast<uint16_t>(uvID);
+                            if (normalID >= 0)
+                            {
+                                currentFace.normalID[vertexIndex] = static_cast<uint16_t>(normalID);
+                                vertexIndex++;
+                            }
+                        }
+                    }
                 }
+                faces.push_back(currentFace);
+            }
+            
+        }
+        std::vector<Vertex> vertices;
+        std::vector<uint16_t> indices;
+
+        for (int i = 0; i < faces.size(); i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int posID = faces[i].vertexID[j], uvID = faces[i].uvID[j], normalID = faces[i].normalID[j];
+
+                Vertex vertex;
+                vertex.Pos = vertexPos[posID - 1];
+                vertex.TexC = uv[uvID - 1];
+                //vertex.normal = normal[normalID - 1];
+
+                vertices.push_back(vertex);
+                indices.push_back(vertices.size() - 1);
             }
         }
 
@@ -43,7 +98,7 @@ namespace hlt_ModelImporter
         // Corrige l'erreur E0153 (expression doit avoir un type classe mais elle a le type "D3DApp *")
         if (D3DApp::GetApp() != nullptr)
         {
-            D3DApp::GetApp()->CreateOriginalMesh(path, vertices, indices);
+            D3DApp::GetApp()->CreateOriginalMesh("path", vertices, indices);
         }
 
         return;
